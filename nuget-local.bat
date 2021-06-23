f9519bf86e7e7b62244c42bfcc2bac26195d026e
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-06-23 09:00:33 +0200

    NuGet all packages

--- nuget-local.bat
+++ nuget-local.bat
@@ -1,1 +1,3 @@
 nuget init .\src\Playwright.Contrib.Extensions\bin\Release\ .\packages
+nuget init .\src\Playwright.Contrib.FluentAssertions\bin\Release\ .\packages
+nuget init .\src\Playwright.Contrib.PageObjects\bin\Release\ .\packages
